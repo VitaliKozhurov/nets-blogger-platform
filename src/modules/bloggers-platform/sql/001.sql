8995a1fa650820db28ad33e558b21f0cea1830a5
VitaliKozhurov <vitalik24021992@gmail.com>
2026-05-12 21:39:14 +0300

    feat: add tables for comments and likes

--- src/modules/bloggers-platform/sql/001.sql
+++ src/modules/bloggers-platform/sql/001.sql
@@ -32,3 +32,71 @@ CREATE TABLE public.posts
 
 ALTER TABLE IF EXISTS public.posts
     OWNER to "nest-admin";
+
+CREATE TABLE public.comments
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    content text NOT NULL,
+    "postId" uuid NOT NULL,
+    "ownerId" uuid NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+    "deletedAt" timestamp with time zone DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY ("postId")
+        REFERENCES public.posts (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    FOREIGN KEY ("ownerId")
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE IF EXISTS public.comments
+    OWNER to "nest-admin";
+
+
+CREATE TABLE public.post_likes
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    "userId" uuid NOT NULL,
+    "postId" uuid NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+    status text NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT uniq_post_likes UNIQUE ("userId", "postId"),
+    FOREIGN KEY ("postId")
+        REFERENCES public.posts (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY ("userId")
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS public.post_likes
+    OWNER to "nest-admin";
+
+
+CREATE TABLE public.comment_likes
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    "userId" uuid NOT NULL,
+    "commentId" uuid NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
+    status like_status_type NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE ("userId", "commentId"),
+    FOREIGN KEY ("userId")
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY ("commentId")
+        REFERENCES public.comments (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS public.comment_likes
+    OWNER to "nest-admin";
